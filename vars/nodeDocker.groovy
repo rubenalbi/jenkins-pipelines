a3af3741e0332eb8cc716b63cbcd0fd5171de91b
Rubén Albiach <ruben.albiach@gmail.com>
2023-09-30 18:41:05 +0200

    parameters

--- vars/nodeDocker.groovy
+++ vars/nodeDocker.groovy
@@ -82,6 +82,11 @@ def call() {
         }
         stage('Deploy') {
             steps {
+                script {
+                    env.RELEASE_SCOPE = input message: 'User input required', ok: 'Release!',
+                            parameters: [choice(name: 'RELEASE_SCOPE', choices: 'patch\nminor\nmajor', description: 'What is the release scope?')]
+                }
+                echo "${env.RELEASE_SCOPE}"
                 sshagent(credentials : ['aws-nginx-server']){
                         sh 'ssh -tt -o StrictHostKeyChecking=no $SSH_CONNECTION ls -l'
                         sh 'ssh $SSH_CONNECTION docker ps'
